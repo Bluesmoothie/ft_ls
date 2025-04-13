ff1bbeaf5e9e9e631bfa204e8d1ff4929d7979b2
Yves Gille <ygille@student.42lyon.fr>
2025-04-13 21:04:24 +0200

    Norm

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 18:10:17 by ygille            #+#    #+#             */
-/*   Updated: 2025/04/13 20:56:53 by ygille           ###   ########.fr       */
+/*   Updated: 2025/04/13 21:03:34 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static t_lslst2	*extract_content(DIR *dir);
 static t_lslst2	*sort_content(t_lslst2 *content, t_param param);
 static bool		sort_helper(const char *s1, const char *s2, t_param param);
+static void		swaper(t_lslst2 **prev, t_lslst2 **content);
 
 void	ls_path(t_context ctx, char *arg)
 {
@@ -82,10 +83,7 @@ static t_lslst2	*sort_content(t_lslst2 *content, t_param param)
 			prev = NULL;
 		}
 		else
-		{
-			prev = content;
-			content = content->next;
-		}
+			swaper(&prev, &content);
 	}
 	return (mem);
 }
@@ -96,11 +94,17 @@ static bool	sort_helper(const char *s1, const char *s2, t_param param)
 		return (true);
 	if (!ft_strncmp(s1, ".", 1) && s1[1])
 		s1++;
-	if (!ft_strncmp(s2, ".", 1)&& s2[1])
+	if (!ft_strncmp(s2, ".", 1) && s2[1])
 		s2++;
 	if (!param.reverse && ft_strcmp_casei(s1, s2) > 0)
 		return (true);
 	else if (param.reverse && ft_strcmp_casei(s1, s2) < 0)
 		return (true);
 	return (false);
+}
+
+static void	swaper(t_lslst2 **prev, t_lslst2 **content)
+{
+	*prev = *content;
+	*content = (*content)->next;
 }
--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 19:46:45 by ygille            #+#    #+#             */
-/*   Updated: 2025/04/13 20:48:54 by ygille           ###   ########.fr       */
+/*   Updated: 2025/04/13 21:00:43 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,9 @@ static void	print_longformat(t_lslst2 *content, t_param param)
 				ft_printf("\n");
 			else
 				first = false;
-			ft_printf("%c%s %d %s %s %d %s", content->mode, content->perms, content->links, content->owner, content->group, content->size, content->name);
+			ft_printf("%c%s %d %s %s %d %s", content->mode, content->perms,
+				content->links, content->owner, content->group, content->size,
+				content->name);
 		}
 		content = content->next;
 	}
